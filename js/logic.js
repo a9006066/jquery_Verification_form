8b14856f759f76aa5f06212ed0b8e17ba7bafc73
AlvinChen <AlvinChen@transglobe.com.tw>
2019-04-08 15:17:29 +0800

    調整

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,3 @@
-var datas = getDatas($); //費率資料
-
 /** 
 @parDays	    --要保天數(1~180)
 @parPG		   	--選擇 AD.個人保險 AG.團體保險
@@ -13,127 +11,14 @@ var datas = getDatas($); //費率資料
 @relage       --實際年齡
 @startdt	    --投保始期(民國年月日)	*/
 
+var datas  
 
-var logic = function(rbtnInfo) {
+// 初始化
 
-  var i ;//差異天數
-  var parDays = rbtnInfo.parDays;//要保天數
-  var parMoney = rbtnInfo.parMoney;//保額
-  var borndtt = turnStarTdd(rbtnInfo.borndt); //出生年月日
-  var startdd = turnStarTdd(rbtnInfo.startdt); //投保始期西元年
-  var enddt = turnEnddt(rbtnInfo.startdt,parDays); //投保結束日
-  var age = rbtnInfo.relage; //實際年齡
-  var fullage = borndtt;//滿15歲的年月日 borndtt + 150000
-  var vfullage = fullage;
-  var parPG = changePG(age,rbtnInfo.parPG); //險種
-  var parAddPol = rbtnInfo.parAddPol;//附加傷害醫療
-  var adbpm;//附加保費
-  var adbpm2;//附加保費
-
-  var array = getAdbpm (parAddPol,parMoney,parDays,);//保費計算
-
-
-/**
- **保費計算
- **/
-function getAdbpm (){
-
-  //附加傷害醫療(1=5%,2=10%)
-  if(parAddPol==='1' || parAddPol==='2'){
-
-    //MR(MG)保額有0.5萬,則先*2再/2
-    if(parAddPol==='1' && parMoney*0.05*10%10 === 5){
-
-     var v = 0.05*2;
-
-     var adbpmTemp = datas.filter((_item)=>{
-        return _item.TADAY === parDays && (((parPG === 'AE'|| parPG === 'AF') && _item.TACODE ==='MS')
-        || ((parPG === 'AJ'|| parPG === 'AK') && _item.TACODE ==='MF')) && _item.bsf === parMoney*v ;
-      });
-
-     var adbpmSUM = adbpmTemp.reduce((accumulator, currentValue) => accumulator + currentValue);
-
-     return adbpm = Math.round(adbpmSUM/2);
-    }
-  //正常狀態
-  else{
-
-     var v = parAddPol === '1'? 0.05 : 0.1;
-
-      var adbpmTemp = datas.filter((_item)=>{
-        return _item.TADAY === parDays && (((parPG === 'AE'|| parPG === 'AF') && _item.TACODE ==='MS')
-        || ((parPG === 'AJ'|| parPG === 'AK') && _item.TACODE ==='MF')) && _item.bsf === parMoney*v ;
-      }) ;
-
-    var adbpmSUM = adbpmTemp.reduce((accumulator, currentValue) => accumulator + currentValue);
-
-    return adbpm = Math.round(adbpmSUM/2);
-    }
-
-  }// if
-
-  //--傷害醫療+海外疾病住院醫療(3=5%,4=10%)
-  else if(parAddPol==='3' || parAddPol==='4'){
-
-    //MR(MG)保額有0.5萬,則先*2再/2
-   if(parAddPol==='3' && parMoney*0.05*10%10 === 5){
-     
-    var v = 0.05*2;
-
-    var adbpmTemp = datas.filter((_item)=>{
-      return _item.TADAY === parDays && (((parPG === 'AE'|| parPG === 'AF') && _item.TACODE ==='MS')
-      || ((parPG === 'AJ'|| parPG === 'AK') && _item.TACODE ==='MF')) && _item.bsf === parMoney*v ;
-    }) ;
-
-    var adbpmSUM = adbpmTemp.reduce((accumulator, currentValue) => accumulator + currentValue);
-    adbpm = Math.round(adbpmSUM/2);
-
-
-    var adbpm2TempA = datas.filter((_item)=>{
-      return _item.TADAY === parDays && (parPG === 'SM' && _item.bsf === parMoney*0.05-0.5) 
-    })[0].BPM ; 
-
-    var adbpm2TempB = datas.filter((_item)=>{
-      return _item.TADAY === parDays && (parPG === 'SM' && _item.bsf === parMoney*0.05+0.5) 
-    })[0].BPM ; 
-
-    adbpm2 = (adbpm2TempA + adbpm2TempB)/2 ;
-
-    return adbpm + Math.round(adbpm2);
+(function () { 
+  getDatas($); //費率資料 
+})()
   
-   }
-   //--正常狀態
-   else { 
-
-    var v = parAddPol ==='3'? 0.05: 0.1;
-
-    var adbpmTemp = datas.filter((_item)=>{
-      return _item.TADAY === parDays && (((parPG === 'AE' || parPG === 'AF') && (_item.TACODE ==='MS'||_item.TACODE ==='SM'))
-      || ((parPG === 'AJ'|| parPG === 'AK') &&(_item.TACODE ==='MF' ||_item.TACODE ==='SM'))) && _item.bsf === parMoney*v ;
-    }) ;
-
-    var adbpmSUM = adbpmTemp.reduce((accumulator, currentValue) => accumulator + currentValue);
-    
-    // adbpm = Math.round(adbpmSUM/2);
-
-   }
-
-
-
-  }
-
-
-
-
-
-}
-
-
-
-
-};
-
-
 
 /**
  **轉換險種
@@ -150,7 +35,7 @@ function changePG (age,parPG){
  **/
 function getDatas($) {
   $.get("data.json", function(response) {
-    return response;
+    datas = response;
   });
 }
 
@@ -170,3 +55,142 @@ function turnEnddt(date,days) {
  dat.setDate(dat.getDate() + parseInt(days));
  return dat;
 }
+
+function logic(rbtnInfo) {
+
+  var i ;//差異天數
+  var parDays = rbtnInfo.parDays;//要保天數
+  var parMoney = rbtnInfo.parMoney;//保額
+  var borndtt = turnStarTdd(rbtnInfo.borndt); //出生年月日
+  var startdd = turnStarTdd(rbtnInfo.startdt); //投保始期西元年
+  var enddt = turnEnddt(rbtnInfo.startdt,parDays); //投保結束日
+  var age = rbtnInfo.relage; //實際年齡
+  var fullage = borndtt;//滿15歲的年月日 borndtt + 150000
+  var vfullage = fullage;
+  var parPG = changePG(age,rbtnInfo.parPG); //險種
+  var parAddPol = rbtnInfo.parAddPol;//附加傷害醫療
+  var adbpm;//附加保費
+  var adbpm2;//附加保費
+  
+  return getAdbpm ();//保費計算
+  
+
+/**
+ **保費計算
+ **/
+function getAdbpm (){
+
+  //附加傷害醫療(1=5%,2=10%) start
+  if(parAddPol==='1' || parAddPol==='2'){
+
+    //MR(MG)保額有0.5萬,則先*2再/2
+    if(parAddPol==='1' && parMoney*0.05*10%10 === 5){
+
+     var v = 0.05*2;
+
+     var adbpmTemp = datas.filter((_item)=>{
+        return _item.TADAY === parDays && (((parPG === 'AE'|| parPG === 'AF') && _item.TACODE ==='MS')
+        || ((parPG === 'AJ'|| parPG === 'AK') && _item.TACODE ==='MF')) && _item.BSF === parMoney*v ;
+      });
+
+      var adbpmSUM = 0;
+      
+     adbpmTemp.forEach((_item)=> {adbpmSUM += _item.BPM;});
+
+     return adbpm = Math.round(adbpmSUM/2);
+    }
+  //正常狀態
+  else{
+
+     var v = parAddPol === '1'? 0.05 : 0.1;
+
+      var adbpmTemp = datas.filter((_item)=>{
+        return _item.TADAY.toString() === parDays && (((parPG === 'AE'|| parPG === 'AF') && _item.TACODE ==='MS')
+        || ((parPG === 'AJ'|| parPG === 'AK') && _item.TACODE ==='MF')) && _item.BSF === parMoney*v ;
+      }) ;
+
+      var adbpmSUM = 0;
+
+      adbpmTemp.forEach((_item)=> {adbpmSUM += _item.BPM;});
+      
+      return adbpm = Math.round(adbpmSUM/2);
+    }
+
+  }//附加傷害醫療(1=5%,2=10%) end
+
+
+  //--傷害醫療+海外疾病住院醫療(3=5%,4=10%) start
+  else if(parAddPol==='3' || parAddPol==='4'){
+
+    //MR(MG)保額有0.5萬,則先*2再/2
+   if(parAddPol==='3' && parMoney*0.05*10%10 === 5){
+     
+    var v = 0.05*2;
+
+    var adbpmTemp = datas.filter((_item)=>{
+      return _item.TADAY.toString() === parDays && (((parPG === 'AE'|| parPG === 'AF') && _item.TACODE ==='MS')
+      || ((parPG === 'AJ'|| parPG === 'AK') && _item.TACODE ==='MF')) && _item.BSF === parMoney*v ;
+    }) ;
+
+    var adbpmSUM = 0;
+      
+    adbpmTemp.forEach((_item)=> {adbpmSUM += _item.BPM;});
+
+    adbpm = Math.round(adbpmSUM/2);
+
+
+    var adbpm2TempA = datas.filter((_item)=>{
+      return _item.TADAY.toString() === parDays && (parPG === 'SM' && _item.BSF === parMoney*0.05-0.5) 
+    })[0].BPM ; 
+
+    var adbpm2TempB = datas.filter((_item)=>{
+      return _item.TADAY.toString() === parDays && (parPG === 'SM' && _item.BSF === parMoney*0.05+0.5) 
+    })[0].BPM ; 
+
+    adbpm2 = (adbpm2TempA + adbpm2TempB)/2 ;
+
+    return adbpm + Math.round(adbpm2);
+  
+   }
+   //--正常狀態
+   else { 
+
+    var v = parAddPol ==='3'? 0.05: 0.1;
+
+    var adbpmTemp = datas.filter((_item)=>{
+      return _item.TADAY.toString() === parDays && (((parPG === 'AE' || parPG === 'AF') && (_item.TACODE ==='MS' || _item.TACODE ==='SM'))
+      || ((parPG === 'AJ' || parPG === 'AK') &&( _item.TACODE ==='MF' || _item.TACODE ==='SM'))) && _item.BSF === parMoney*v ;
+    }) ;
+
+    var adbpmSUM = 0;
+      
+    adbpmTemp.forEach((_item)=> {adbpmSUM += _item.BPM;});
+
+    return adbpm = Math.round(adbpmSUM/2);
+
+   }
+
+  } //傷害醫療+海外疾病住院醫療(3=5%,4=10%) end
+
+
+   //無
+
+  // var adbpmTemp = datas.filter((_item)=>{
+  //   return _item.TADAY === parDays && (((parPG === 'AE'|| parPG === 'AF') && _item.TACODE ==='MS')
+  //   || ((parPG === 'AJ'|| parPG === 'AK') && _item.TACODE ==='MF')) && _item.bsf === parMoney*v ;
+  // }) ;
+
+
+
+
+
+
+
+}//getAdbpmend
+
+
+
+
+
+
+};//logic end
